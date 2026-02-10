2a13f4a63658c4b063626060a786044f13b60d05
Dixita-Chauhan <tapuchauhan50@gmail.com>
2026-02-10 08:58:05 +0530

    Implement basic numpy array operations
    Add numpy array manipulations and print statements

---
+++ Day 5/02.py
@@ -0,0 +1,13 @@
+import numpy as np
+arr=np.array([1,2,3])
+arr[2]=10
+print(arr[2])
+print(type(arr[2]))
+
+arr2=np.array([
+  [1,2,3]
+  [4,5,np.nan]
+  ])
+print(arr2)
+print(arr2[1][1])
+#slicing
