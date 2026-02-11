dac608cde11cf7efa72d3e062791d93f2cd11a57
Dixita-Chauhan <tapuchauhan50@gmail.com>
2026-02-11 08:19:14 +0530

    Add functions for cube, double, triple, power, and login

--- Day 5/04.py
+++ Day 5/04.py
@@ -1,6 +1,34 @@
+#1
 def findsquare(n):
   return n*n
+#2  
 def findcube(n):
   return n*n*n
-
-
+#3
+def finddouble(n):
+    return n * 2
+#4
+def findtriple(n):
+    return n * 3
+#5
+def findpower(n, p):
+    return n ** p
+#6
+def iseven(n):
+    return n % 2 == 0
+#6
+def isodd(n):
+    return n % 2 != 0
+ 
+#7
+def login(username, password):
+    if username == "abc" and password == "1234":
+        return "Login successful"
+    else:
+        return "Invalid username or password"
+#8
+def check_username(user):
+    return len(user) >= 4
+#9
+def check_password_length(pwd):
+    return len(pwd) >= 6
